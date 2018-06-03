c230ecf7dc40486815da03321f8ccabd93f6bbe0
Damian Kubiak <kubiak.damian96@gmail.com>
2018-06-03 15:04:21 +0200

    QuizCompleted layout changed

--- src/Quizes/QuizCompleted.js
+++ src/Quizes/QuizCompleted.js
@@ -52,17 +52,17 @@ class QuizCompleted extends Component {
         }else{
             return (
                 <div>
-                    <div>
-                        {this.state.questions.map((i,index) => 
-                            <div>
-                                <div>{i.question}</div>
-                                <div>{this.state.answers[index]}</div>
-                            </div>
-                        )}
-                    </div>
                     <Points>
                         Uzyskałeś {this.props.match.params.points} / {this.props.match.params.maxPoints} punktów!
                     </Points>
+                    <QuestionsAndAnswers className = "col-lg-6 offset-lg-3">
+                        {this.state.questions.map((i,index) => 
+                            <SingleSet>
+                                <Question>{i.question}</Question>
+                                <Answer>{this.state.answers[index]}</Answer>
+                            </SingleSet>
+                        )}
+                    </QuestionsAndAnswers>                
                 </div>
             );
         }
@@ -74,5 +74,25 @@ export default QuizCompleted;
 const Points = styled.div`
     text-align: center;
     font-family: 'Indie Flower', cursive;
+    font-size: 5vh;
+`
+
+const QuestionsAndAnswers = styled.div`
+    margin-top: 5vh;
+    text-align: center;
+`
+const SingleSet = styled.div`
+    margin-top: 3vh;
+`
+
+const Question = styled.div`
+    font-family: 'Indie Flower', cursive;
     font-size: 3vh;
+`
+
+const Answer = styled.div`
+    font-family: 'Indie Flower', cursive;
+    font-weight: bold;
+    color: rgb(0, 163, 19);
+    font-size: 2vh;
 `
