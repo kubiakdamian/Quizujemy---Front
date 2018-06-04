ab32281e169cd5258197a429c45b8f3a7a3c812d
Damian Kubiak <kubiak.damian96@gmail.com>
2018-06-04 03:19:59 +0200

    Working app v1

--- src/UserPanel.js
+++ src/UserPanel.js
@@ -7,18 +7,21 @@ import axios from "axios";
 import Circle from 'react-circle';
 import { callToast } from "./user-interface/alert";
 import { toast } from "react-toastify";
+import curiosityImage from "./images/curiosity4.jpg";
 
 class UserPanel extends Component {
     constructor(props) {
         super(props);
         this.state = {
             statistics: {},
-            curiosity: ""
+            curiosity: "",
+            anterooms: []
         };
     }
 
     componentDidMount(){
         this.getStatistics();
+        this.getAnterooms();
     }
 
     getStatistics = () => {
@@ -31,6 +34,19 @@ class UserPanel extends Component {
           this.setState({
             statistics: response.data[0]
           })
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+
+    getAnterooms = () => {
+        axios
+        .get(`http://localhost:8080/anteroom/curiosities`)
+        .then(response => {
+            this.setState({
+                anterooms: response.data
+            })
         })
         .catch(error => {
           console.log(error);
@@ -57,7 +73,7 @@ class UserPanel extends Component {
     }
 
     getPercentage = () => {
-        if(this.state.statistics === undefined){
+        if(this.state.statistics !== undefined){
             if(this.state.statistics.answeredQuestions === 0){
                 return 0;
             }else{
@@ -80,12 +96,13 @@ class UserPanel extends Component {
         });
       }
 
-    addCuriosity = () => {
+    addAnteroom = () => {
         axios
         .post(`http://localhost:8080/anteroom/add`, {content: this.state.curiosity})
         .then(response => {
             this.clearContent();
             callToast("Pomyślnie dodano ciekawostkę");
+            this.getAnterooms();
         })
         .catch(error => {
           console.log(error);
@@ -93,68 +110,128 @@ class UserPanel extends Component {
         });
     }
 
+    deleteAnteroom = id => {
+        axios
+        .delete(`http://localhost:8080/anteroom/delete/${id}`)
+        .then(response => {
+            this.getAnterooms();
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+
+    addCuriosity = (newContent, id) => {
+        axios
+        .post(`http://localhost:8080/curiosities/add`, {content: newContent})
+        .then(response => {
+            this.deleteAnteroom(id);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+
     render() {
-        console.log(this.state.statistics);
-        return (
-            <div>
-                <Text style={{fontSize: "4vh"}}>
-                    Witaj w panelu użytkownika {this.props.user.email}!
-                </Text>
-                <Text>
-                    Oto Twoje statystyki:
-                </Text>
-                <CircleContainer className="col-lg-2 offset-lg-5">
-                    <Circle
-                        animate={true}
-                        responsive={true}
-                        size={150}
-                        lineWidth={50}
-                        progress={this.getPercentage()}
-                        progressColor="rgb(132, 21, 34)" 
-                        bgColor="whitesmoke"
-                        textColor="black"
-                        textStyle={{ 
-                            textAlign: "center",
-                            fontFamily: 'Indie Flower',
-                            fontSize: '8vh',
-                            color: 'black'
-                        }}
-                        percentSpacing={10}
-                        roundedStroke={true}
-                        showPercentage={true}
-                        showPercentageSymbol={true}
-                    />
-                </CircleContainer>
-                <Text>{this.state.statistics.correctAnswers} / {this.state.statistics.answeredQuestions} poprawnych odpowiedzi</Text>
-                <Text>
-                    Dowiedziałeś się czegoś ciekawego? Podziel się tym z nami, a może trafisz na stronę główną!
-                </Text>
-                <InputContainer className="col-lg-6 offset-lg-3">
-                    <div className="form-group">
-                        <label for="exampleFormControlTextarea1">Twoja ciekawostka</label>
-                        <textarea 
-                            className="form-control"
-                            id="exampleFormControlTextarea1"
-                            rows="3"
-                            onChange={this.updateContent}
-                            value={this.state.curiosity} />
+        console.log(this.state.anterooms);
+        if(this.props.user.role === 2){
+            if(this.state.statistics === undefined){
+                return(<div>Loading...</div>);
+            }else{
+                return (
+                    <div>
+                        <Text style={{fontSize: "4vh"}}>
+                            Witaj w panelu użytkownika {this.props.user.email}!
+                        </Text>
+                        <Text>
+                            Oto Twoje statystyki:
+                        </Text>
+                        <CircleContainer className="col-lg-2 offset-lg-5">
+                            <Circle
+                                animate={true}
+                                responsive={true}
+                                size={150}
+                                lineWidth={50}
+                                progress={this.getPercentage()}
+                                progressColor="rgb(132, 21, 34)" 
+                                bgColor="whitesmoke"
+                                textColor="black"
+                                textStyle={{ 
+                                    textAlign: "center",
+                                    fontFamily: 'Indie Flower',
+                                    fontSize: '8vh',
+                                    color: 'black'
+                                }}
+                                percentSpacing={10}
+                                roundedStroke={true}
+                                showPercentage={true}
+                                showPercentageSymbol={true}
+                            />
+                        </CircleContainer>
+                        <Text>{this.state.statistics.correctAnswers} / {this.state.statistics.answeredQuestions} poprawnych odpowiedzi</Text>
+                        <Text>
+                            Dowiedziałeś się czegoś ciekawego? Podziel się tym z nami, a może trafisz na stronę główną!
+                        </Text>
+                        <InputContainer className="col-lg-6 offset-lg-3">
+                            <div className="form-group">
+                                <label for="exampleFormControlTextarea1">Twoja ciekawostka</label>
+                                <textarea 
+                                    className="form-control"
+                                    id="exampleFormControlTextarea1"
+                                    rows="3"
+                                    onChange={this.updateContent}
+                                    value={this.state.curiosity} />
+                            </div>
+                            <CuriosityButtonContainer>
+                                <StyledButton
+                                    style={{backgroundColor: "rgb(43, 124, 255)"}}
+                                    onClick={this.addAnteroom}               
+                                    label={"Wyślij"}
+                                />
+                            </CuriosityButtonContainer>
+                        </InputContainer>
+                        <LogoutButtonContainer className="col-lg-2 offset-lg-5">
+                            <StyledButton
+                                onClick={this.logout}               
+                                label={"Wyloguj się"}
+                            />
+                        </LogoutButtonContainer>            
                     </div>
-                    <CuriosityButtonContainer>
+                );
+            }
+        }else{
+            return(
+                <div>
+                    <Text>
+                        Witaj admin! Oto nowe ciekawostki:
+                    </Text>
+                    {this.state.anterooms.map((i, index)=> 
+                        <Anteroom className="col-lg-6 offset-lg-3">
+                            {i.content}
+                            <AnteroomButtons className="col-lg-6 offset-lg-3">
+                                <StyledButton
+                                    style={{width: "50%", backgroundColor: "rgb(0, 155, 57)"}}
+                                    onClick={() => this.addCuriosity(i.content, i.idanteroom)}            
+                                    label={"Dodaj"}
+                                />
+                                <StyledButton
+                                    style={{width: "50%"}}
+                                    onClick={() => this.deleteAnteroom(i.idanteroom)}               
+                                    label={"Usuń"}
+                                />
+                            </AnteroomButtons>
+                        </Anteroom>
+                    )}
+                    <LogoutButtonContainer className="col-lg-2 offset-lg-5">
                         <StyledButton
-                            style={{backgroundColor: "rgb(43, 124, 255)"}}
-                            onClick={this.addCuriosity}               
-                            label={"Wyślij"}
+                            onClick={this.logout}               
+                            label={"Wyloguj się"}
                         />
-                    </CuriosityButtonContainer>
-                </InputContainer>
-                <LogoutButtonContainer className="col-lg-2 offset-lg-5">
-                    <StyledButton
-                        onClick={this.logout}               
-                        label={"Wyloguj się"}
-                    />
-                </LogoutButtonContainer>            
-            </div>
-        );
+                    </LogoutButtonContainer>  
+                </div>
+            );
+        }
+        
     }
 }
 
@@ -198,5 +275,19 @@ const CircleContainer = styled.div`
 const InputContainer = styled.div`
   margin-top: 5vh;
   margin-bottom: 10vh;
+`
+
+const Anteroom = styled.div`
+    margin-top: 3vh;
+    font-size: 2.5vh;
+    color: white;
+    background-image: url(${curiosityImage});
+    background-repeat: no-repeat;
+    background-size: cover;
+`
+
+const AnteroomButtons = styled.div`
+  margin-top: 7vh;
+  padding-bottom: 2vh;
 `
 
